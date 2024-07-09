4e350052896de59d435ac37f8b8737c5ea4f63d9
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-07-10 05:22:21 +1000

    Create game.lua

---
+++ game.lua
@@ -0,0 +1,60 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "nigga of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local RunService = game:GetService("RunService")
+
+local player = game.Players.LocalPlayer
+
+-- tabs
+local Tab = Window:MakeTab({
+    Name = "Tab 1",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+-- notify
+OrionLib:MakeNotification({
+    Name = "hi!",
+    Content = "welcome",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+})
+
+-- pet spawner
+
+game:GetService("ReplicatedStorage").Remotes.CanBuyEgg:InvokeServer("Air Jinn",false)
+OrionLib:Init()
+-- button
+
+Tab:AddButton({
+    Name = "pet!",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.CanBuyEgg:InvokeServer("Air Jinn",false)
+    end
+})
+
+--toggle button
+Tab:AddToggle({ Name = "goodfarm!", Default = false, Flag = "autofarm" }) 
+
+RunService.RenderStepped:Connect(function()
+    if OrionLib.Flags['autofarm'].Value then 
+        local orbs = game:GetService("Workspace").GameAssets.GlobalAssets.OrbSpawns
+
+        for i, v in pairs(orbs:GetChildren()) do
+            if v:IsA("MeshPart") then
+                if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+                    player.Character.HumanoidRootPart.CFrame = v.CFrame
+                end
+            elseif v:IsA("Model") then
+                local summerOrb = v:FindFirstChild("Orb. 1")
+                if summerOrb then
+
+                    if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+                        player.Character.HumanoidRootPart.CFrame = summerOrb.CFrame
+                    end
+                end
+            end
+        end
+    end
+end)
